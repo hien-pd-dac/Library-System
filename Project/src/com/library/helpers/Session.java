f3fd2a6c04888a484b8c6159593df8cbf8f3347c
Nguyen Hanh <hanhnv.bkhn@gmail.com>
2017-11-20 20:42:35 +0700

    hanhnv.view.fix1

--- Project/src/com/library/helpers/Session.java
+++ Project/src/com/library/helpers/Session.java
@@ -5,10 +5,34 @@
  */
 package com.library.helpers;
 
+import java.util.HashMap;
+import java.util.Map;
 /**
  *
  * @author Ronaldo Hanh
  */
 public class Session {
+    private static Map<String, String> session = new HashMap();
     
+    public static boolean exist(String key){
+        return session.containsKey(key);
+    }
+    
+    public static void add(String key, String value){
+        if(!value.isEmpty()){
+            session.put(key, value);
+        }
+    }
+    
+    public static String get(String key){
+        return session.get(key);
+    }
+    
+    public static String remove(String key){
+        return session.remove(key);
+    }
+    
+    public static void destroy(){
+        session.clear();
+    }
 }
